6869ebbde932f020c01ad896104a5935250cb055
LinaSobs <linaesobikela@gmail.com>
2026-02-03 04:10:44 +0200

    Add files via upload

---
+++ ProJECt AD 202/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace ProJECt_AD_202
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tblApplication = new System.Windows.Forms.TableLayoutPanel();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.RichTextBox();
+            this.tblApplication.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(557, 378);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 43);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Firebrick;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(677, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Silver;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(12, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(776, 43);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "STUDENT APPLICATION FORM";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tblApplication
+            // 
+            this.tblApplication.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tblApplication.ColumnCount = 2;
+            this.tblApplication.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 41.80523F));
+            this.tblApplication.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 58.19477F));
+            this.tblApplication.Controls.Add(this.txtCourse, 1, 5);
+            this.tblApplication.Controls.Add(this.txtGender, 1, 3);
+            this.tblApplication.Controls.Add(this.txtAge, 1, 2);
+            this.tblApplication.Controls.Add(this.txtSurname, 1, 1);
+            this.tblApplication.Controls.Add(this.lblName, 0, 0);
+            this.tblApplication.Controls.Add(this.lblSurname, 0, 1);
+            this.tblApplication.Controls.Add(this.lblAge, 0, 2);
+            this.tblApplication.Controls.Add(this.lblGender, 0, 3);
+            this.tblApplication.Controls.Add(this.lblAddress, 0, 4);
+            this.tblApplication.Controls.Add(this.lblCourse, 0, 5);
+            this.tblApplication.Controls.Add(this.txtName, 1, 0);
+            this.tblApplication.Controls.Add(this.txtAddress, 1, 4);
+            this.tblApplication.Location = new System.Drawing.Point(36, 131);
+            this.tblApplication.Name = "tblApplication";
+            this.tblApplication.RowCount = 6;
+            this.tblApplication.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.70589F));
+            this.tblApplication.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.70588F));
+            this.tblApplication.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.70588F));
+            this.tblApplication.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.70588F));
+            this.tblApplication.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.47059F));
+            this.tblApplication.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.70588F));
+            this.tblApplication.Size = new System.Drawing.Size(495, 288);
+            this.tblApplication.TabIndex = 4;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.BackColor = System.Drawing.Color.Silver;
+            this.txtCourse.Location = new System.Drawing.Point(210, 247);
+            this.txtCourse.MaxLength = 100;
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(265, 26);
+            this.txtCourse.TabIndex = 10;
+            // 
+            // txtGender
+            // 
+            this.txtGender.BackColor = System.Drawing.Color.Silver;
+            this.txtGender.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtGender.Location = new System.Drawing.Point(210, 130);
+            this.txtGender.MaxLength = 1;
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(44, 26);
+            this.txtGender.TabIndex = 8;
+            // 
+            // txtAge
+            // 
+            this.txtAge.BackColor = System.Drawing.Color.Silver;
+            this.txtAge.Location = new System.Drawing.Point(210, 88);
+            this.txtAge.MaxLength = 2;
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(62, 26);
+            this.txtAge.TabIndex = 7;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.BackColor = System.Drawing.Color.Silver;
+            this.txtSurname.Location = new System.Drawing.Point(210, 46);
+            this.txtSurname.MaxLength = 50;
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(170, 26);
+            this.txtSurname.TabIndex = 6;
+            // 
+            // lblName
+            // 
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(4, 1);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(199, 32);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSurname.Location = new System.Drawing.Point(4, 43);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(199, 32);
+            this.lblSurname.TabIndex = 1;
+            this.lblSurname.Text = "Surname";
+            // 
+            // lblAge
+            // 
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(4, 85);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(199, 32);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "Age";
+            // 
+            // lblGender
+            // 
+            this.lblGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblGender.Location = new System.Drawing.Point(4, 127);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(199, 32);
+            this.lblGender.TabIndex = 3;
+            this.lblGender.Text = "Gender (M/F)";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblAddress.Location = new System.Drawing.Point(4, 169);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(199, 32);
+            this.lblAddress.TabIndex = 4;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCourse.Location = new System.Drawing.Point(4, 244);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(199, 32);
+            this.lblCourse.TabIndex = 5;
+            this.lblCourse.Text = "Course";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.Silver;
+            this.txtName.Location = new System.Drawing.Point(210, 4);
+            this.txtName.MaxLength = 50;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(170, 26);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.BackColor = System.Drawing.Color.Silver;
+            this.txtAddress.Location = new System.Drawing.Point(210, 172);
+            this.txtAddress.MaxLength = 100;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(265, 68);
+            this.txtAddress.TabIndex = 11;
+            this.txtAddress.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tblApplication);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tblApplication.ResumeLayout(false);
+            this.tblApplication.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TableLayoutPanel tblApplication;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.RichTextBox txtAddress;
+    }
+}
+
---
+++ ProJECt AD 202/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProJECt_AD_202
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source=YK-LAPTOP/SQLEXPRESS;Initial Catalog=StudentDatabase;Integrated Security=True");
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtName.Text != "" && txtSurname.Text != "" && txtAge.Text != "" && txtGender.Text != "" && txtAddress.Text != "" && txtCourse.Text != "")
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("insert into student values(@StdName, @StdSurname, @Age, @Gender, @Address, @Course)", conn);
+                    cmd.Parameters.AddWithValue("@StdName", txtName.Text);
+                    cmd.Parameters.AddWithValue("@StdSurname", txtSurname.Text);
+                    cmd.Parameters.AddWithValue("@Age", txtAge.Text);
+                    cmd.Parameters.AddWithValue("@Gender", txtGender.Text);
+                    cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+                    cmd.Parameters.AddWithValue("@Course", txtCourse.Text);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Application submitted successfully.");
+
+                }
+
+                else
+                {
+                    MessageBox.Show("ERROR101");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtSurname.Text = "";
+            txtAge.Text = "";
+            txtGender.Text = "";
+            txtAddress.Text = "";
+            txtCourse.Text = "";
+        }
+    }
+}
